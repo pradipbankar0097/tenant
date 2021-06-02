42630c9ded8148e269dcfb80240917d92476a8d1
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-06-02 14:54:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          don't Learn React
         </a>
       </header>
     </div>
